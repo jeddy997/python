ca332a6fa47373d87302127e0f9bc7c80406da03
jeddy997 <rosemuyoka547@gmail.com>
2026-01-30 20:55:35 +0300

    Add files via upload

---
+++ grading.py
@@ -0,0 +1,10 @@
+#sum of 5 subjects scores
+sub1=int(input("enter the scores for subject 1"))
+sub2=int(input("enter the scores for subject 2"))
+sub3=int(input("enter the score for subject 3"))
+sub4=int(input("enter the scores for subject 4")) 
+sub5=int(input("enter the scoes for subject 5"))
+sum=sub1+sub2+sub3+sub4+sub5
+average=sum/5
+print(" the sum for 5 subjects is:",sum)
+print(" the average for 5 subjects:", average)
